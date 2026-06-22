20a8b334a57516b7b3a60dcab4cea5c2291429ae
910862KUMAR <145100981+910862KUMAR@users.noreply.github.com>
2026-06-22 14:37:06 +0530

    Update vector_store.py

--- src/vector_store.py
+++ src/vector_store.py
@@ -1,19 +1,11 @@
-import shutil
-import os
 from langchain_community.vectorstores import Chroma
-from src.config import CHROMA_DB_DIR
 
 
 def create_vector_store(chunks, embeddings):
 
-    # Delete old database before creating a new one
-    if os.path.exists(CHROMA_DB_DIR):
-        shutil.rmtree(CHROMA_DB_DIR)
-
     db = Chroma.from_documents(
         documents=chunks,
-        embedding=embeddings,
-        persist_directory=CHROMA_DB_DIR
+        embedding=embeddings
     )
 
     return db
