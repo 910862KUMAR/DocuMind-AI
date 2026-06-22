f71c1dc628fe5431fc1c128b78f01791d3df692a
910862KUMAR <145100981+910862KUMAR@users.noreply.github.com>
2026-06-22 14:43:52 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,12 +1,14 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
+# Local development
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
-CHROMA_DB_DIR = "chroma_db"
-
-EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
+# Streamlit Cloud
+if not GEMINI_API_KEY:
+    GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
 
 LLM_MODEL = "gemini-2.5-flash"
